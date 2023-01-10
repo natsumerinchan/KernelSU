5185c4068983023574e0731c8b3dc8928d83219d
セリカ・シルフィル <64072399+natsumerinchan@users.noreply.github.com>
2023-01-10 20:56:09 +0800

    Update setup.sh

--- kernel/setup.sh
+++ kernel/setup.sh
@@ -2,18 +2,19 @@
 
 set -x
 
+rm -rf ./KernelSU
 git clone https://github.com/tiann/KernelSU
 
 GKI_ROOT=$(pwd)
 
 echo "[+] GKI_ROOT: $GKI_ROOT"
-echo "[+] Copy kernel su driver to $GKI_ROOT/common/drivers"
+echo "[+] Copy kernel su driver to $GKI_ROOT/drivers"
 
-ln -sf $(pwd)/KernelSU/kernel $GKI_ROOT/common/drivers/kernelsu
+ln -sf $(pwd)/KernelSU/kernel $GKI_ROOT/drivers/kernelsu
 
 echo "[+] Add kernel su driver to Makefile"
 
-DRIVER_MAKEFILE=$GKI_ROOT/common/drivers/Makefile
+DRIVER_MAKEFILE=$GKI_ROOT/drivers/Makefile
 grep -q "kernelsu" $DRIVER_MAKEFILE || echo "obj-y += kernelsu/" >> $DRIVER_MAKEFILE
 
 echo "[+] Done."
