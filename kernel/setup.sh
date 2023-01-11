7e945a5919440f6b0f2d3a8fe19977d57d63ffe3
セリカ・シルフィル <64072399+natsumerinchan@users.noreply.github.com>
2023-01-11 15:59:42 +0800

    Update setup.sh

--- kernel/setup.sh
+++ kernel/setup.sh
@@ -2,27 +2,33 @@
 
 set -x
 
-git clone https://github.com/tiann/KernelSU
-
 GKI_ROOT=$(pwd)
 
 echo "[+] GKI_ROOT: $GKI_ROOT"
 
 if test -d "$GKI_ROOT/common/drivers"; then
-     echo "[+] Copy kernel su driver to $GKI_ROOT/common/drivers"
-     ln -sf $(pwd)/KernelSU/kernel $GKI_ROOT/common/drivers/kernelsu
-     echo "[+] Add kernel su driver to Makefile"
-     DRIVER_MAKEFILE=$GKI_ROOT/common/drivers/Makefile
-     grep -q "kernelsu" $DRIVER_MAKEFILE || echo "obj-y += kernelsu/" >> $DRIVER_MAKEFILE
-     echo "[+] Done."
+     DRIVER_DIR="$GKI_ROOT/common/drivers"
 elif test -d "$GKI_ROOT/drivers"; then
-     echo "[+] Copy kernel su driver to $GKI_ROOT/drivers"
-     ln -sf $(pwd)/KernelSU/kernel $GKI_ROOT/drivers/kernelsu
-     echo "[+] Add kernel su driver to Makefile"
-     DRIVER_MAKEFILE=$GKI_ROOT/drivers/Makefile
-     grep -q "kernelsu" $DRIVER_MAKEFILE || echo "obj-y += kernelsu/" >> $DRIVER_MAKEFILE
-     echo "[+] Done."
+     DRIVER_DIR="$GKI_ROOT/drivers"
 else
-     echo "[ERROR] "drivers/" folder is not found."
+     echo "[ERROR] "drivers/" directory is not found."
      echo "[INFO] You should modify this scrpit by yourself."
+     exit 127
 fi
+
+test -d "$GKI_ROOT/KernelSU" || git clone https://github.com/tiann/KernelSU
+cd "$GKI_ROOT/KernelSU"
+git stash && git pull
+cd "$GKI_ROOT"
+
+echo "[+] GKI_ROOT: $GKI_ROOT"
+echo "[+] Copy kernel su driver to $DRIVER_DIR"
+
+ln -sf "$GKI_ROOT/KernelSU/kernel" $DRIVER_DIR/kernelsu
+
+echo "[+] Add kernel su driver to Makefile"
+
+DRIVER_MAKEFILE=$DRIVER_DIR/Makefile
+grep -q "kernelsu" $DRIVER_MAKEFILE || echo "obj-y += kernelsu/" >> $DRIVER_MAKEFILE
+
+echo "[+] Done."
